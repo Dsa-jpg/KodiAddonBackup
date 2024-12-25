22fd2ebaaa11d29dd3dc5c62cfca5679a2c6f876
Filip <nachtman.filip@seznam.cz>
2024-12-25 18:34:49 +0100

    Refactor action handling by introducing a router function for improved code organization and maintainability

--- default.py
+++ default.py
@@ -11,6 +11,7 @@ from resources.lib.dialog_utils import dialog_handler, dialog_notify
 from resources.lib.actions import handle_search, handle_most_watched, play_episode, select_streams, show_episodes, show_seasons, top_films, trending_shows
 from resources.lib.trakt import TraktClient
 from resources.lib.config import TRAKTLOGIN
+from resources.lib.router import router
 
 # Inicializace
 addon_handle = int(sys.argv[1])
@@ -43,66 +44,4 @@ if ( int(my_addon.getSetting('expiretime')) - int(time.time())) < 0:
     my_addon.setSetting('refreshtoken',tokens[0])
     my_addon.setSetting('expiretime', tokens[1])
 
-# Směrování akcí
-if action == 'search':
-    handle_search(webC, my_addon)
-
-elif action == 'most_watched':
-    
-    handle_most_watched(webC,addon_handle,my_addon)
-
-elif action == 'select_stream':
-
-    select_streams(params)
-
-
-elif action == 'topfilms':
-
-    top_films(traK,TRAKTLOGIN.CLIENTID,webC,my_addon,addon_handle)
-
-elif action == 'trendingshows':
-
-    trending_shows(traK,TRAKTLOGIN.CLIENTID,webC,my_addon,addon_handle)
-
-elif action == 'list_seasons':
-    show_id = params.get('show_id')
-    show_seasons(traK, TRAKTLOGIN.CLIENTID, show_id, addon_handle)
-
-elif action == 'list_episodes':
-    show_id = params.get('show_id')
-    season_number = int(params.get('season'))
-    show_episodes(traK, TRAKTLOGIN.CLIENTID, show_id, season_number, addon_handle)
-
-elif action == 'play_episode':
-    url = params.get('url')
-    play_episode(url)
-
-else:
-    # Výchozí obsah
-    xbmcplugin.setContent(addon_handle, 'movies')
-
-    # Přidání položky "Search"
-    search_url = f'plugin://plugin.video.helloworld/?action=search'
-    search_li = xbmcgui.ListItem('Search')
-    search_li.setInfo('video', {'title': 'Search for movies or series'})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=search_url, listitem=search_li, isFolder=False)
-
-    # Přidání položky "Most Watched"
-    most_watched_url = f'plugin://plugin.video.helloworld/?action=most_watched'
-    most_watched_li = xbmcgui.ListItem('Most Watched')
-    most_watched_li.setInfo('video', {'title': 'Most watched movies'})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=most_watched_url, listitem=most_watched_li, isFolder=True)
-
-    #  Pridani polozky "Top 50 films"
-    topfilms_url = f'plugin://plugin.video.helloworld/?action=topfilms'
-    topfilms_li = xbmcgui.ListItem('Top Films')
-    topfilms_li.setInfo('video', {'title': 'Top 50 films all time'})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=topfilms_url, listitem=topfilms_li, isFolder=True)
-
-    # trending shows
-    topfilms_url = f'plugin://plugin.video.helloworld/?action=trendingshows'
-    topfilms_li = xbmcgui.ListItem('Trending shows')
-    topfilms_li.setInfo('video', {'title': 'Top 50 Trending shows'})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=topfilms_url, listitem=topfilms_li, isFolder=True)
-
-    xbmcplugin.endOfDirectory(addon_handle)    
+router(action, params, traK, TRAKTLOGIN, webC, my_addon, addon_handle)
---
+++ resources/lib/router.py
@@ -0,0 +1,51 @@
+import uuid
+from .actions import handle_search, handle_most_watched, select_streams, top_films, trending_shows, show_seasons, show_episodes, play_episode
+import xbmcplugin
+import xbmcgui
+
+
+def router(action, params, traK, TRAKTLOGIN, webC, my_addon, addon_handle):
+    # Definice směrování jako slovník, kde klíče jsou akce a hodnoty jsou funkce
+    actions = {
+        'search': lambda: handle_search(webC, my_addon),
+        'most_watched': lambda: handle_most_watched(webC, addon_handle, my_addon),
+        'select_stream': lambda: select_streams(params),
+        'topfilms': lambda: top_films(traK, TRAKTLOGIN.CLIENTID, webC, my_addon, addon_handle),
+        'trendingshows': lambda: trending_shows(traK, TRAKTLOGIN.CLIENTID, webC, my_addon, addon_handle),
+        'list_seasons': lambda: show_seasons(traK, TRAKTLOGIN.CLIENTID, params.get('show_id'), addon_handle),
+        'list_episodes': lambda: show_episodes(webC,traK, TRAKTLOGIN.CLIENTID, params.get('show_id'), int(params.get('season')), addon_handle, my_addon.getSetting('token'), str(uuid.uuid4())),
+        'play_episode': lambda: play_episode(params.get('url')),
+    }
+
+    # Pokud akce existuje v našem slovníku, zavoláme ji
+    if action in actions:
+        actions[action]()
+    else:
+        # Výchozí obsah, pokud akce není známa
+        xbmcplugin.setContent(addon_handle, 'movies')
+
+        # Přidání položky "Search"
+        search_url = f'plugin://plugin.video.helloworld/?action=search'
+        search_li = xbmcgui.ListItem('Search')
+        search_li.setInfo('video', {'title': 'Search for movies or series'})
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=search_url, listitem=search_li, isFolder=False)
+
+        # Přidání položky "Most Watched"
+        most_watched_url = f'plugin://plugin.video.helloworld/?action=most_watched'
+        most_watched_li = xbmcgui.ListItem('Most Watched')
+        most_watched_li.setInfo('video', {'title': 'Most watched movies'})
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=most_watched_url, listitem=most_watched_li, isFolder=True)
+
+        # Přidání položky "Top 50 films"
+        topfilms_url = f'plugin://plugin.video.helloworld/?action=topfilms'
+        topfilms_li = xbmcgui.ListItem('Top Films')
+        topfilms_li.setInfo('video', {'title': 'Top 50 films all time'})
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=topfilms_url, listitem=topfilms_li, isFolder=True)
+
+        # Přidání položky "Trending shows"
+        trendingshows_url = f'plugin://plugin.video.helloworld/?action=trendingshows'
+        trendingshows_li = xbmcgui.ListItem('Trending shows')
+        trendingshows_li.setInfo('video', {'title': 'Top 50 Trending shows'})
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=trendingshows_url, listitem=trendingshows_li, isFolder=True)
+
+        xbmcplugin.endOfDirectory(addon_handle)
